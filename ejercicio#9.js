4f9e838324921fb6bf4b47fbfef53d38fa05fb73
= <=>
2026-07-02 03:37:44 -0500

    ECHO#9

---
+++ ejercicio#9.js
@@ -0,0 +1,12 @@
+var vendedor = {
+    nombre: "Yordan"
+};
+var sueldo_de_Yordan = 1200000;
+var comision_de_Yordan = 0.10; // 10% de comisión
+var ventas_de_Yordan = 2000000;
+var ventas_de_Yordan = 2000000;
+var ventas_de_Yordan = 2000000;
+vendedor.sueldo = sueldo_de_Yordan;
+vendedor.comision = comision_de_Yordan * ventas_de_Yordan;
+
+console.log("el sueldo de " + vendedor.nombre + " es: " + vendedor.sueldo + " y su comisión es: " + vendedor.comision); 
