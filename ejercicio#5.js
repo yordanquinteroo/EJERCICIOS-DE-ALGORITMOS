fad9b9ae8df60d468918a1caa7a85d6cbdbf2dc1
= <=>
2026-07-02 02:19:57 -0500

    ECHO#5

---
+++ ejercicio#5.js
@@ -0,0 +1,6 @@
+var distancia_auto = 10000; // kilometros
+var time = 2; // horas
+
+var velocidad_auto = distancia_auto / time;
+
+console.log("La velocidad del auto es: " + velocidad_auto + " km/h");
