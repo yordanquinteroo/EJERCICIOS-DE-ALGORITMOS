d244bfa9fcfaa47c339b640c4a39ef34ede2c586
= <=>
2026-07-02 03:05:58 -0500

    ECHO#7

---
+++ ejercicio#7.js
@@ -0,0 +1,7 @@
+var hora = 20; // horas
+var minutos = 30; // minutos
+var segundos =45; // segundos
+
+var totalSegundos = (hora * 3600) + (minutos * 60) + segundos;
+
+console.log("El total de segundos es: " + totalSegundos);
