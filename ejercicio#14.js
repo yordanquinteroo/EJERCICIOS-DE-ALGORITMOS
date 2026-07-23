2e9067fa31c9f5b711cb9fe7869e31b096f6e0fc
= <=>
2026-07-23 18:14:57 -0500

    ECHO#14

---
+++ ejercicio#14.js
@@ -0,0 +1,6 @@
+var base = parseFloat(prompt("Ingrese la base del triángulo:"));
+var altura = parseFloat(prompt("Ingrese la altura del triángulo:"));
+
+var area = (base * altura) / 2;
+
+console.log("El área del triángulo es: " + area);
