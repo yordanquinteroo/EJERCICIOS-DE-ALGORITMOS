13bc90a9d7a4e0fe7c89477b09b0c82d1f99d0c5
= <=>
2026-07-23 18:15:42 -0500

    ECHO#15

---
+++ ejercicio#15.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat(prompt("Ingrese la primera nota:"));
+var nota2 = parseFloat(prompt("Ingrese la segunda nota:"));
+var nota3 = parseFloat(prompt("Ingrese la tercera nota:"));
+
+var promedio = (nota1 + nota2 + nota3) / 3;
+
+console.log("El promedio es: " + promedio);
