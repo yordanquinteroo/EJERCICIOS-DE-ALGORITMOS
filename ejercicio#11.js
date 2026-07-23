8aaf9367738315d0c0d8019a167a1a8902d0bae9
= <=>
2026-07-23 17:53:48 -0500

    ejercicio#11

--- ejercicio#11.js
+++ ejercicio#11.js
@@ -1,5 +1,13 @@
-var num1 = 10
-var num2 = 30 
-var num3 = 80
+const prompt = require("prompt-sync")();
 
-console.log 
+let parcial1 = parseFloat(prompt("Ingrese la nota del primer parcial: "));
+let parcial2 = parseFloat(prompt("Ingrese la nota del segundo parcial: "));
+let parcial3 = parseFloat(prompt("Ingrese la nota del tercer parcial: "));
+let examen = parseFloat(prompt("Ingrese la nota del examen final: "));
+let trabajo = parseFloat(prompt("Ingrese la nota del trabajo final: "));
+
+let promedio = (parcial1 + parcial2 + parcial3) / 3;
+
+let notaFinal = (promedio * 0.55) + (examen * 0.30) + (trabajo * 0.15);
+
+console.log("La calificación final es: " + notaFinal.toFixed(2));
